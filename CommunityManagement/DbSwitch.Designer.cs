11ba3e0479fd9094746e8fdcf540e6789374f77c
tinygrox <vinnie32@outlook.com>
2019-12-29 03:16:35 +0800

    一个更改

--- CommunityManagement/DbSwitch.Designer.cs
+++ CommunityManagement/DbSwitch.Designer.cs
@@ -215,6 +215,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.LightSkyBlue;
             this.ClientSize = new System.Drawing.Size(286, 358);
             this.Controls.Add(this.statusStrip1);
